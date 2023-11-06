2063e234eab2c17680c65ee58ed1ab007d397f54
polosa00 <i@apolochanin.ru>
2023-11-06 18:01:33 +0300

    add new files

---
+++ CrimeFilesApp/CrimeFilesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeFilesAppApp.swift
+//  CrimeFilesApp
+//
+//  Created by Александр Полочанин on 6.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CrimeFilesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
