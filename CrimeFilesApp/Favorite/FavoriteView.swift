9884e9c3e27a7f63f3e2416c1b6b60e66e1da9c9
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-11-23 17:09:57 +0300

    Add FavoriteView

--- CrimeFilesApp/Favorite/FavoriteView.swift
+++ CrimeFilesApp/Favorite/FavoriteView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct FavoriteView: View {
+    let title = "Избранное"
+    let sections = ["Favorite1", "Favorite2","Favorite3","Favorite4"]
+    let imageTitle = "mockImage"
+    
     var body: some View {
-        ZStack {
+        CustomNavView{
             Color("Main")
                 .ignoresSafeArea()
             VStack {
-                Text("Избранное здесь")
-                    .foregroundColor(.white)
-                
+                ScrollView {
+                    ForEach(sections, id: \.self) { section in
+                        CustomNavLink (
+                            destination:
+                                ContentView(title: section, mainText: section)
+                                .customNavigationTitle("Custom \(section)")
+                        )
+                        {  // Start custom Label here
+                            CellView(title: section, imageName: imageTitle)
+                        } // End Custom label here
+                        .padding(.horizontal, 16)
+                        
+                    }
+                }
             }
+            .customNavigationFavoriteButtonHidden(true)
+            .customNavigationTitle(title)
+            .customNavigationBarBackButtonHidden(true)
         }
     }
 }
